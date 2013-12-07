518ac5ccebb76ccf9ac6d245c3c912d8460e8c1a
Mihail Velikov <mihailvelik@gmail.com>
2013-12-08 00:52:16 +0200

    create results table view controller

---
+++ GPSResultsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  GPSResultsTableViewController.h
+//  GooglePlacesSearch
+//
+//  Created by mihata on 12/8/13.
+//  Copyright (c) 2013 mihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GPSResultsTableViewController : UITableViewController
+
+@end
