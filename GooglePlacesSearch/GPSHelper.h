0e87a14842a8dec6cae34376a458abde230cfe8f
Mihail Velikov <mihailvelik@gmail.com>
2013-12-11 23:13:28 +0200

    add helper class with methods sending http request to API

---
+++ GooglePlacesSearch/GPSHelper.h
@@ -0,0 +1,19 @@
+//
+//  VCHelper.h
+//  VChat
+//
+//  Created by mihata on 11/28/13.
+//  Copyright (c) 2013 mihata. All rights reserved.
+//
+
+#define kOFFSET_FOR_KEYBOARD 200.0
+
+#import <Foundation/Foundation.h>
+
+@interface GPSHelper : NSObject
+
++(NSURLRequest*) sendSimpleHTTPRequestFor: (NSString*)url withStringData: (NSString*)stringData;
++(NSURLRequest*) sendSimpleHTTPRequestForUrlAddress: (NSString*)url WithLatitude: (NSString*) latitude Longitude: (NSString*) longitude AndTypes: (NSArray*) types;
++(void) showAlertMessageWithTitle: (NSString*) title andText: (NSString*) text;
+
+@end
---
+++ GooglePlacesSearch/GPSHelper.m
@@ -0,0 +1,50 @@
+//
+//  VCHelper.m
+//  VChat
+//
+//  Created by mihata on 11/28/13.
+//  Copyright (c) 2013 mihata. All rights reserved.
+//
+
+#import "GPSHelper.h"
+
+@implementation GPSHelper
+
++(NSURLRequest*) sendSimpleHTTPRequestFor: (NSString*)url withStringData: (NSString*)stringData {
+    NSURL *fullUrl = [NSURL URLWithString:[@"https://maps.googleapis.com/maps/api/" stringByAppendingString:url]];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:fullUrl];
+    [request setHTTPMethod:@"POST"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"content-type"];
+    
+    NSData *data = [stringData dataUsingEncoding:NSUTF8StringEncoding];
+    [request setHTTPBody:data];
+    [request setValue:[NSString stringWithFormat:@"%u", [data length]] forHTTPHeaderField:@"Content-Length"];
+    return request;
+}
+
++(NSURLRequest*)sendSimpleHTTPRequestForUrlAddress: (NSString*)url WithLatitude: (NSString*) latitude Longitude: (NSString*) longitude AndTypes: (NSArray*) types {
+    if ([latitude length] == 0 || [longitude length] == 0) {
+        [NSException raise:@"Invalid location provided" format:@"Please switch your GPS!"];
+    }
+    
+    NSString* typesString = [@"&types=" stringByAppendingString: [types componentsJoinedByString:@"&"]];
+    NSString* locationString = [[[@"location=" stringByAppendingString:latitude]
+                                 stringByAppendingString:@","]
+                                 stringByAppendingString:longitude];
+    NSString* data = [[locationString stringByAppendingString:@"&rankby=distance&sensor=false"]
+                      stringByAppendingString:typesString];
+    return [self sendSimpleHTTPRequestFor:@"place/nearbysearch/json?" withStringData:data];
+}
+
++(void) showAlertMessageWithTitle:(NSString *)title andText:(NSString *)text {
+    UIAlertView *alertView = [[UIAlertView alloc ] initWithTitle:title
+                                                         message:text
+                                                        delegate:nil
+                                               cancelButtonTitle:@"OK"
+                                               otherButtonTitles:nil, nil];
+    
+    [alertView show];
+}
+
+@end
